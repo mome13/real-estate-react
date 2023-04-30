1ac1b6edf6cc8971e1761d2f5869f3d6cdad469d
mehran13mome <mehran13mome@gmail.com>
2023-04-30 17:51:13 +0330

    feat/ui: Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/scenes/navbar";
+import Footer from "@/scenes/footer";
 
 function App() {
   return (
     <div className="app bg-gray-20">
       <Navbar />
+      <Footer />
     </div>
   );
 }
