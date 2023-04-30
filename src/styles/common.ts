446c9d864a4767d9d3881902cb3c290aa47cff9b
mehran13mome <mehran13mome@gmail.com>
2023-04-30 21:05:43 +0330

    feat/ui: separate common styles

---
+++ src/styles/common.ts
@@ -0,0 +1,5 @@
+export const container = "container mx-auto flex px-5";
+export const primaryBtn =
+  "hidden rounded-xl bg-blue-500 px-6 py-2 text-sm font-bold text-white transition duration-200 hover:bg-blue-600 lg:inline-flex";
+export const secondaryBtn =
+  "hidden rounded-xl bg-gray-100 px-6 py-2 text-sm font-bold text-gray-900 transition duration-200 hover:bg-gray-200  lg:ml-auto lg:mr-3 lg:inline-block";
