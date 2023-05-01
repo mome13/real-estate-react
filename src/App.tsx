db0d8886bbd83b7f0c369ee527b2e8855f827c28
mehran13mome <mehran13mome@gmail.com>
2023-05-01 19:04:52 +0330

    feat/ui: Add routes to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Home from "@/scenes/home";
+import AboutUs from "@/scenes/aboutUs";
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="about" element={ <AboutUs/> } />
+        {/* <Route path="contact" element={ <Contact/> } /> */}
+      </Routes>
       <Footer />
     </div>
   );
