05e55e4b35c62ac072bceb647408cd2cf37a9e40
mehran13mome <mehran13mome@gmail.com>
2023-05-02 18:55:03 +0330

    feat/ui: add search page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Home from "@/scenes/home";
 import AboutUs from "@/scenes/aboutUs";
-import { Routes, Route } from "react-router-dom"
+import Search from "@/scenes/search/search";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="about" element={ <AboutUs/> } />
-        {/* <Route path="contact" element={ <Contact/> } /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<AboutUs />} />
+        <Route path="search" element={<Search />} />
       </Routes>
       <Footer />
     </div>
