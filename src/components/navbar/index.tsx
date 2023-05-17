ec032fe95738ab81898a9e9fda6157720726ca35
mehran13mome <mehran13mome@gmail.com>
2023-05-18 02:28:05 +0330

    update/ui: update navbar padding

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -101,7 +101,7 @@ const Navbar = (props: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
     <>
-      <nav className={`${flexBetween} w-full bg-white`}>
+      <nav className={`${flexBetween} w-full bg-white py-6`}>
         <div className={`${flexBetween} container mx-auto px-5`}>
           <a className="text-3xl font-bold leading-none" href="#">
             <svg className="h-10" viewBox="0 0 10240 10240">
