55bd614530c579f1a7cafc16133c0933756b2be8
mehran13mome <mehran13mome@gmail.com>
2023-05-18 02:29:30 +0330

    feat/logic: add general click handler

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ import AboutUs from "@/scenes/aboutUs";
 import Search from "@/scenes/search/search";
 import { Routes, Route } from "react-router-dom";
 
+type HTMLElementEvent<T extends HTMLElement> = Event & {
+  target: T;
+};
 function App() {
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function (event: any) {
+    if (!event.target?.matches(".dropbtn") && !event.target?.closest(".dropdown-content")) {
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (!openDropdown.classList.contains("hidden")) {
+          openDropdown.classList.add("hidden");
+        }
+      }
+    }
+  };
   return (
     <div className="app">
       <Navbar />
