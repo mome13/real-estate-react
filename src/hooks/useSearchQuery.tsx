bc62ec2749042caa6a2b9f5c56d01208e30478d1
mehran13mome <mehran13mome@gmail.com>
2023-07-28 00:33:50 +0330

    Add useSearchQuery hook

--- src/hooks/useSearchQuery.tsx
+++ src/hooks/useSearchQuery.tsx
@@ -1,25 +1,31 @@
 import axios, { AxiosResponse } from "axios";
 import dispatch from "@/api/client";
 import { useQuery } from "react-query";
+import { IProperty } from "@/scenes/property";
 
-const fetchSearchItems = () => {
+const fetchSearchItems = ({ queryKey }) => {
   return dispatch({
     method: "get",
-    url: "/property/find",
-    params: {
-      // type: "sale",
-      bathrooms: 3,
-    },
+    url: `/api/property/find${queryKey[1] ? `?${queryKey[1]}` : ""}`,
+    // params: {
+    // type: "sale",
+    //   bathrooms: 3,
+    // },
   });
 };
 export const useSearchQuery = (
-  onSuccess?: (data: AxiosResponse<any, any>) => void,
+  queryParams: string | null,
+  // onSuccess?: (data: AxiosResponse<any, any>) => void,
   onError?: (data: AxiosResponse<any, any>) => void,
   select?: (data: AxiosResponse<any, any>) => any
 ) => {
-  return useQuery("search", fetchSearchItems, {
-    onSuccess,
-    onError,
-    select,
-  });
+  return useQuery<any, any, Array<IProperty>, any>(
+    ["search", queryParams],
+    fetchSearchItems,
+    {
+      // onSuccess,
+      onError,
+      select,
+    }
+  );
 };
